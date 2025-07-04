2d108140c419f093c690d6f840db63014216a186
Aayush Rathore <aayu.r.2003@gmail.com>
2025-07-04 20:46:48 +0530

    ✅ Back-end setup completed

---
+++ apps/back-end/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './module/auth/auth.module';
+import { UserModule } from './module/user/user.module';
+
+@Module({
+  imports: [AuthModule, UserModule],
+})
+export class AppModule {}
