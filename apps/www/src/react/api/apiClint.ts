d88fe9b02177d6c4ae1291ce405d539e479be9e2
Nihal1487 <chaudharynihal09@gmail.com>
2025-07-27 16:43:05 +0530

    bug fix

--- apps/www/src/react/api/apiClint.ts
+++ apps/www/src/react/api/apiClint.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL || "https://api.prqr.in",
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://api.prqr.in",
+  // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
